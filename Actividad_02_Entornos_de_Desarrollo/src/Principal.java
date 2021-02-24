caa6095cc659b9b9fe9b9bf3862d6b5fee153793
esm980 <vve20bis@gmail.com>
2021-02-24 22:19:27 +0100

    creando clase principal

---
+++ Actividad_02_Entornos_de_Desarrollo/src/Principal.java
@@ -0,0 +1,10 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Producto pro1 = new Producto();
+		
+			System.out.println("El resultado de la multiplicación de dos números pasados por pantallas es " + pro1.multiplicar(2, 7));
+	}
+
+}
