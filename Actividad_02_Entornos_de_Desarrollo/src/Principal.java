dd71bf1f0410199658fa84759e8a599c4b473561
@ñarly <carrabmin@gmail.com>
2021-02-24 23:20:34 +0100

    Prueba del método factorial de la clase Operacion en la clase Principal

--- Actividad_02_Entornos_de_Desarrollo/src/Principal.java
+++ Actividad_02_Entornos_de_Desarrollo/src/Principal.java
@@ -3,8 +3,11 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Producto pro1 = new Producto();
+		Operacion op1 = new Operacion();
 		
 			System.out.println("El resultado de la multiplicación de dos números pasados por pantallas es " + pro1.multiplicar(2, 7));
+			
+			System.out.println("El resultado de aplicar el método factorial a 5 debería ser: 120 -> " + op1.factorial(5));
 	}
 
 }
